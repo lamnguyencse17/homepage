d96aed07fa14f5465c620262e99674233d55891f
Lam Nguyen <nguyenquanglam3008@gmail.com>
2022-09-13 15:32:32 +0700

    Add github sections to about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -13,6 +13,7 @@ import Image from "next/future/image";
 import Link from "next/link";
 import AnimatedPage from "../../components/root/animatedPage";
 import SkillsIcon from "../../public/skills.svg";
+import GithubIcon from "../../public/github.svg";
 import ExperienceIcon from "../../public/experience.svg";
 import { motion } from "framer-motion";
 import Head from "next/head";
@@ -42,7 +43,7 @@ const About: NextPage = () => {
             better
           </Text>
           <Grid
-            templateColumns="repeat(6, 1fr)"
+            templateColumns="repeat(3, 1fr)"
             width="100%"
             gap={10}
             paddingX="10"
@@ -55,7 +56,6 @@ const About: NextPage = () => {
               flexDirection="column"
               as={motion.div}
               whileHover={{ scale: 1.2 }}
-              colStart={2}
             >
               <LinkBox width="100%" height="100%">
                 <Link href="/about/experience" passHref>
@@ -92,7 +92,6 @@ const About: NextPage = () => {
               flexDirection="column"
               as={motion.div}
               whileHover={{ scale: 1.2 }}
-              colStart={5}
             >
               <LinkBox width="100%" height="100%">
                 <Link href="/about/skills" passHref>
@@ -116,6 +115,42 @@ const About: NextPage = () => {
                         textAlign="center"
                       >
                         Skills
+                      </Text>
+                    </Flex>
+                  </LinkOverlay>
+                </Link>
+              </LinkBox>
+            </GridItem>
+            <GridItem
+              width="100%"
+              height="200px"
+              display="flex"
+              flexDirection="column"
+              as={motion.div}
+              whileHover={{ scale: 1.2 }}
+            >
+              <LinkBox width="100%" height="100%">
+                <Link href="/about/github" passHref>
+                  <LinkOverlay>
+                    <Flex direction="column" width="100%" height="100%">
+                      <Box width="100%" flex="1">
+                        <Image
+                          src={GithubIcon}
+                          style={{
+                            height: "100%",
+                            width: "fit-content",
+                            margin: "auto",
+                          }}
+                          alt="Link to github page"
+                        />
+                      </Box>
+
+                      <Text
+                        fontSize="xl"
+                        fontWeight="semibold"
+                        textAlign="center"
+                      >
+                        Github
                       </Text>
                     </Flex>
                   </LinkOverlay>
